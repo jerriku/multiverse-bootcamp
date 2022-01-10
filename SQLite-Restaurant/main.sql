f2d78d778e61ac3e8b4c0a8ef07db87e58377a15
Jeric Paul B. Fajardo <76446904+jerriku@users.noreply.github.com>
2022-01-10 14:26:57 +0000

    Add JOIN of relationships between Restaurants, Menus and MenuItems

--- SQLite-Restaurant/main.sql
+++ SQLite-Restaurant/main.sql
@@ -6,13 +6,17 @@ CREATE TABLE Restaurants (
 
 CREATE TABLE Menus (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT
+  title TEXT, 
+  restaurant_id INTEGER,
+  FOREIGN KEY (restaurant_id) REFERENCES Restaurants(id)
 );
 
 CREATE TABLE MenuItems (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
-  price REAL
+  price REAL,
+  menu_id INTEGER,
+  FOREIGN KEY (menu_id) REFERENCES Menus(id)
 );
 
 INSERT INTO Restaurants (name, imageLink)
@@ -23,31 +27,31 @@ VALUES ("Ippudo", "https://www.ippudo.co.uk/wordpress/wp-content/themes/ippudo-t
 
 SELECT * FROM Restaurants;
 
-INSERT INTO Menus (title)
-VALUES ("Starters");
+INSERT INTO Menus (title, restaurant_id)
+VALUES ("Starters", 1);
 
-INSERT INTO Menus (title)
-VALUES ("PERi-PERi Chicken");
+INSERT INTO Menus (title, restaurant_id)
+VALUES ("PERi-PERi Chicken", 1);
 
-INSERT INTO Menus (title)
-VALUES ("Lunch Set");
+INSERT INTO Menus (title, restaurant_id)
+VALUES ("Lunch Set", 1);
 
-INSERT INTO Menus (title)
-VALUES ("Sharing Platters");
+INSERT INTO Menus (title, restaurant_id)
+VALUES ("Sharing Platters", 1);
 
 SELECT * FROM Menus;
 
-INSERT INTO MenuItems (name, price)
-VALUES ("1/2 Chicken", 7.95);
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("1/2 Chicken", 7.95, 2);
 
-INSERT INTO MenuItems (name, price)
-VALUES ("Chicken Butterfly", 8.25);
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Chicken Butterfly", 8.25, 2);
 
-INSERT INTO MenuItems (name, price)
-VALUES ("Whole Chicken", 14.50);
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Whole Chicken", 14.50, 2);
 
-INSERT INTO MenuItems (name, price)
-VALUES ("10 Chicken Wings", 10.95);
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("10 Chicken Wings", 10.95, 2);
 
 INSERT INTO MenuItems (name, price)
 VALUES ("KARAKA-MEN", 9.50);
@@ -77,3 +81,50 @@ FROM MenuItems;
 
 SELECT AVG(price)
 FROM MenuItems;
+
+
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Rainbow Bowl", 8.95, 3);
+
+INSERT INTO Menus (title, restaurant_id)
+VALUES ("Appetisers", 2);
+
+INSERT INTO Menus (title, restaurant_id)
+VALUES ("Ramen", 2);
+
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Chicken Kara-Age", 5.00, 5);
+
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Age-Tako Yaki", 5.00, 5);
+
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Shiromaru Classic", 11.50, 6);
+
+INSERT INTO MenuItems (name, price, menu_id)
+VALUES ("Akamaru Modern", 12.50, 6);
+
+SELECT Restaurants.name, Menus.title
+FROM Restaurants
+JOIN Menus ON Restaurants.id = Menus.restaurant_id;
+
+SELECT Menus.title, MenuItems.name
+FROM Menus
+JOIN MenuItems ON Menus.id = MenuItems.menu_id;
+
+SELECT  Menus.title, Restaurants.name, MenuItems.name
+FROM Restaurants
+JOIN Menus ON Restaurants.id = Menus.restaurant_id
+JOIN MenuItems ON Menus.id = MenuItems.menu_id
+WHERE Menus.title = "PERi-PERi Chicken";
+
+SELECT Restaurants.name, COUNT(Menus.title)
+FROM Restaurants
+JOIN Menus ON Restaurants.id = Menus.restaurant_id
+GROUP BY Restaurants.name;
+
+SELECT Menus.title, SUM(MenuItems.price)
+FROM Menus
+JOIN MenuItems ON Menus.id = MenuItems.menu_id
+GROUP BY Menus.title
+ORDER BY SUM(MenuItems.price) DESC;
