b829a8ae011466ebb65d12bc3892bcd375be4b14
Jeric Fajardo <jeric.fajardo@playtech.com>
2022-01-11 14:10:19 +0000

    Add DataBase with sqlite3

---
+++ SQLite-Restaurant/checkDB.js
@@ -0,0 +1,41 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./restaurants.db', sqlite3.OPEN_READONLY, err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to the restaurants database.");
+});
+
+try {
+    db.serialize(() => {
+        // db.each("SELECT * FROM RESTAURANTS", (err, rows) => {
+        //     console.log(rows);
+        // });
+        
+        // db.each("SELECT * FROM MENUS", (err, rows) => {
+        //     console.log(rows);
+        // });
+
+        // db.each(`"SELECT * FROM MENUITEMS"`, (err, rows) => {
+        //     console.log(rows);
+        // });
+        
+        db.each(`SELECT MENUS.title, RESTAURANTS.name AS restaurantname, MENUITEMS.name
+                 FROM RESTAURANTS
+                 JOIN MENUS ON RESTAURANTS.id = MENUS.restaurant_id 
+                 JOIN MENUITEMS ON MENUS.id = MENUITEMS.menu_id`, (err, rows) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(rows);
+        });
+    });
+} finally {
+    db.close(err => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Closed the database connection.");
+    })
+}
---
+++ SQLite-Restaurant/initialiseDB.js
@@ -0,0 +1,30 @@
+const sqlite3 = require('sqlite3').verbose();
+
+
+function initialise() {
+    const db = new sqlite3.Database('./restaurants.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to restaurants database.');
+    });
+
+    try {
+        db.serialize(() => {
+            //Creating RESTAURANTS, MENUS and MENUITEMS tables.
+            db.run("CREATE TABLE RESTAURANTS (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, imagelink TEXT)");
+            db.run("CREATE TABLE MENUS (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, restaurant_id INTEGER, FOREIGN KEY (restaurant_id) REFERENCES RESTAURANTS(id))");
+            db.run("CREATE TABLE MENUITEMS (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, menu_id INTEGER, FOREIGN KEY (menu_id) REFERENCES MENUS(id))");
+        });
+    } finally {
+        db.close((err) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log('Closed the database connection.');
+        });
+    }
+
+}
+
+initialise();
