bcdd2fc7a94587fe74b71ef92799ba592acc5b64
Jeric Fajardo <jeric.fajardo@playtech.com>
2022-01-18 20:41:48 +0000

    Update HTTP methods to use express-validator

--- web-server/server.js
+++ web-server/server.js
@@ -3,6 +3,7 @@ const { sequelize } = require('./sequelize/sequelize_index');
 const { Restaurant } = require('./sequelize/classes/Restaurant');
 const { Menu } = require('./sequelize/classes/Menu');
 const { MenuItem } = require('./sequelize/classes/MenuItem');
+const { check, validationResult } = require('express-validator');
 const fs = require('fs').promises;
 
 const app = express();
@@ -62,45 +63,70 @@ app.get("/restaurants/id=:id", async (request, response) => {
     response.json(restaurant);
 });
 
-app.post("/restaurants", async (request, response) => {
-    if (!request.body.name || !request.body.image) return response.sendStatus(400);
-    await Restaurant.create({name: request.body.name, image: request.body.image});
-    response.sendStatus(201);
-});
+app.post('/restaurants', [
+    check('name').trim().notEmpty().isLength({max: 50}).whitelist('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '),
+    check('image').trim().isURL()
+    ], async (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) return response.status(400).json({ errors: errors.array() });
+        await Restaurant.create({name: request.body.name, image: request.body.image});
+        response.sendStatus(201);
+    }
+);
 
-app.post("/restaurants/menus", async (request, response) => {
-    if (!request.body.title || !request.body.restaurant_id) return response.sendStatus(400);
-    await Menu.create({title: request.body.title, restaurant_id: request.body.restaurant_id});
-    response.sendStatus(201);
-});
+app.post('/restaurants/menus', [
+    check('title').trim().notEmpty().isLength({max: 50}),
+    check('restaurant_id').trim().isInt()
+    ], async (request, response) => {
+        console.log(request.body);
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) return response.status(400).json({ errors: errors.array() });
+        await Menu.create({title: request.body.title, restaurant_id: request.body.restaurant_id});
+        response.sendStatus(201);
+    }
+)
 
-app.post("/restaurants/menus/items", async (request, response) => {
-    if (!request.body.name || !request.body.price || !request.body.menu_id) return response.sendStatus(400);
-    await MenuItem.create({name: request.body.name, price: request.body.price, menu_id: request.body.menu_id});
-    response.sendStatus(201);
-});
+app.post("/restaurants/menus/items", [
+    check('name').trim().notEmpty().isLength({max: 50}),
+    check('price').trim().isCurrency({allow_negatives: false}),
+    check('menu_id').trim().isInt()
+    ], async (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) return response.status(400).json({ errors: errors.array() });
+        await MenuItem.create({name: request.body.name, price: request.body.price, menu_id: request.body.menu_id});
+        response.sendStatus(201);
+    }
+);
 
-app.put("/restaurants", async (request, response) => {
-    if (!request.body.id) return response.sendStatus(400);
-    if (request.body.name) await Restaurant.update(
-        { name: request.body.name }, 
-        { where: { id: request.body.id } }
-    );
+app.put("/restaurants", [
+    check('id').trim().isInt()
+    ], async (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) return response.status(400).json({ errors: errors.array() });
+        if (request.body.name) await Restaurant.update(
+            { name: request.body.name }, 
+            { where: { id: request.body.id } }
+        );
 
-    if (request.body.image) await Restaurant.update(
-        { image: request.body.image }, 
-        { where: { id: request.body.id } }
-    );
-    
-    response.sendStatus(201);
-});
+        if (request.body.image) await Restaurant.update(
+            { image: request.body.image }, 
+            { where: { id: request.body.id } }
+        );
+        
+        response.sendStatus(201);
+    }
+);
 
-app.delete("/restaurants", async (request, response) => {
-    if (!request.body.id) return response.sendStatus(400);
-    const restaurant = await Restaurant.findByPk(request.body.id);
-    restaurant.destroy();
-    response.sendStatus(201);
-});
+app.delete("/restaurants", [
+    check('id').trim().isInt()
+    ], async (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) return response.status(400).json({ errors: errors.array() });
+        const restaurant = await Restaurant.findByPk(request.body.id);
+        restaurant.destroy();
+        response.sendStatus(201);
+    }
+);
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
